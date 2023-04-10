ce1decec51e28eb2b5355dcc63d234a98462a531
utkarsht123 <115368725+utkarsht123@users.noreply.github.com>
2023-04-11 01:44:35 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTA5NTA3MzA5NDQ3MDc0MjA0Ng.GBk_o7.SkG-ufeJWR1ePB9g0T0pPYMzo2x6M-0Z34okxc'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
